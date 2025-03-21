ee6b8d7965b0ce4484d2d816f29de6b061ff6793
LishikaBasavaraj <lishikahb@gmail.com>
2025-03-21 08:24:17 +0530

    1

--- ComCastCRMLatestGUIFRameWork/src/main/java/com/comcast/crm/generic/fileutility/IPathConstatnt.java
+++ ComCastCRMLatestGUIFRameWork/src/main/java/com/comcast/crm/generic/fileutility/IPathConstatnt.java
@@ -4,7 +4,7 @@ public interface IPathConstatnt {
 	String DB_URL="jdbc:mysql://49.249.28.218:3333/ninza_hrm";  //ninza hrm
 	String DB_UN="root";
 	String DB_PWD="root";
-	String db_url="jdbc:mysql://localhost:3306/tekpyramid";
+	String db_url="";
 	String commonDataPtah = "./ConfigData/CommonData.properties";
 	String testDataPath = "./TestScriptData/CeateOrg.xlsx";
 	String JSON_path = "./ConfigData/CommonData.json";
